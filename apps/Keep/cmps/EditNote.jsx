6a0da11d4943f959db9dfe501093a46ee6614a44
adisiera <adisiera@gmail.com>
2020-12-24 14:07:21 +0200

    fixed the edit button

--- apps/Keep/cmps/EditNote.jsx
+++ apps/Keep/cmps/EditNote.jsx
@@ -4,7 +4,6 @@ export class EditNote extends React.Component {
 
     state = {
         note: {
-            id: '',
             type: "NoteText",
             isPinned: false,
             info: {
@@ -17,47 +16,47 @@ export class EditNote extends React.Component {
   
 
     componentDidMount() {
-        // console.log('refInput',refInput);
+     
         const { noteId } = this.props.match.params;
-        console.log('noteId', noteId);
-        console.log('this.refInput:', this.refInput);
+        // console.log('noteId', noteId);
+        // console.log('this.refInput:', this.refInput);
         this.refInput.current.focus();
         if (!noteId) return;
         keepService.getById(noteId).then(note => {
-            this.setState({ note });
-        });
+            this.setState({ note })
+        })
     }
 
     onInputChange = (ev) => {
         
+        // const value = ev.target.value;
         const value = (ev.target.type === 'number')? +ev.target.value  : ev.target.value;
         const note = { ...this.state.note }
-        console.log('note', note)
-        console.log('event',ev);
-        note[ev.target.name] = value
-        this.setState({ note })
+        note.info[ev.target.name] = value
+        this.setState({ 
+            note: note 
+        })
     }
 
     onSaveNote = (ev) => {
-        ev.PreventDefault();
-        console.log('event:',ev);
         console.log('entered save note');
+        ev.preventDefault();
         keepService.save(this.state.note)
-            .then(savedNote => {
-                console.log('changes saved',savedNote);
+            .then(note => {
+                console.log('changes saved',note);
                 this.props.history.push('/note')
             })
     }
 
     render() {
         return (
-            <form onSubmit={this.onSaveNote}>
-                <label htmlFor="text">Write a thought:</label>
-                <input type="text"  ref={this.refInput} placeholder="Once upon a time..."
-                    onChange={this.onInputChange} />
-                <button type="submit">{(this.state.note.id) ? 'Update' : 'Add'}</button>
-            </form>
+                <section>
+                    <label htmlFor="text">Write a thought:</label> 
+                    <input value={this.state.note.info.txt} type="text" ref={this.refInput}  placeholder="Once upon a time..."
+                        name="txt" onChange={this.onInputChange} />
+                    <button onClick={this.onSaveNote}>{(this.state.note.id) ? 'Update' : 'Add'}</button>
+                </section>
+   
         )
     }
-//value={this.state.note.info.txt} 
 }
