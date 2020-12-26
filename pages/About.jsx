233d72a96a4afa9b53ac98701f4eadb2a9411cac
adisiera <adisiera@gmail.com>
2020-12-26 23:16:50 +0200

    added style in About and Mail pages

--- pages/About.jsx
+++ pages/About.jsx
@@ -4,6 +4,10 @@ export function About() {
             <h2>About us...</h2>
             <p>Dina and Adi embarked on a rewarding adventure together. Accompanied by their cats, Foxy and Rumpus, they sought to make the loveliest app for your online life. </p>
             <p>Please enjoy AppSus, the app that brings your world to you!</p>
+            <div className="img-container">
+                <img src="../apps/Keep/assets/img/Foxy.jpeg" />
+                <img src="../apps/Keep/assets/img/Boromir.jpeg" />
+            </div>
         </section>
     )
 }
